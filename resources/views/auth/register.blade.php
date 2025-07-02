edea2e9b5458d6f4827d158688fa4a2f26e5ffae
argusblack242@gmail.com <argusblack242@gmail.com>
2025-07-02 16:47:01 +0100

    last finition

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -124,7 +124,6 @@
                     <option value="etudiant">Étudiant</option>
                     <option value="formateur">Formateur</option>
                     <option value="parent">Parent</option>
-                    <option value="admin">Administrateur</option>
                 </select>
             </div>
 
